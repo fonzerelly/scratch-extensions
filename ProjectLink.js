486486103f5a1317bad6fad3ab89c2622a610284
fonzerelly <ChristianHoerauf@web.de>
2019-12-20 10:57:56 +0100

    default class copied from https://github.com/LLK/scratch-vm/blob/develop/docs/extensions.md

--- ProjectLink.js
+++ ProjectLink.js
@@ -1,1 +1,12 @@
-console.log('ProjectLink')
+class SomeBlocks {
+    constructor (runtime) {
+        /**
+         * Store this for later communication with the Scratch VM runtime.
+         * If this extension is running in a sandbox then `runtime` is an async proxy object.
+         * @type {Runtime}
+         */
+        this.runtime = runtime;
+    }
+
+    // ...
+}
